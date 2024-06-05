8e4e5153842b28e7067b30ff98c4a6f67b682514
Panyut Sriwirote <mildpanyut@gmail.com>
2024-06-05 21:27:18 +0700

    add more results

--- Experiment/treebank/evaluate.py
+++ Experiment/treebank/evaluate.py
@@ -7,12 +7,19 @@ def evaluate_conllu(*, pred_path: str, gold_path: str):
     total = 0
     with open(pred_path, encoding="utf-8") as pred_file,\
         open(gold_path, encoding="utf-8") as gold_file:
-        for pred, gold in zip(pred_file, gold_file):
+        while True:
+            pred, gold = pred_file.readline(), gold_file.readline()
+            if pred == gold == '':
+                break
+            if gold.startswith('#') and not pred.startswith('#'):
+                while gold.startswith('#'):
+                    gold = gold_file.readline()
             if pred == '\n' or pred.startswith('#'):
                 assert pred == gold, f"File mismatch:\n{pred}\n{gold}"
                 continue
-            _, _, _, _, _, _, arg_pred, label_pred, _, _ = pred.split('\t')
-            _, _, _, _, _, _, arg_gold, label_gold, _, _ = gold.split('\t')
+            pred_id, pred_form, _, _, _, _, arg_pred, label_pred, _, _ = pred.split('\t')
+            gold_id, gold_form, _, _, _, _, arg_gold, label_gold, _, _ = gold.split('\t')
+            assert pred_id == gold_id and pred_form == gold_form, f"File mismatch:\n{pred_id}\t{pred_form}\n{gold_id}\t{gold_form}"
             total += 1
             if arg_pred == arg_gold:
                 arg_correct += 1
